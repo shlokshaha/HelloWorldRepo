0c14c65bdebb7485a0dbd96aa5616a70d6fea9cf
Shrikrushna S Zirape <61311396+shrikrushnazirape@users.noreply.github.com>
2020-10-02 09:24:26 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Addition program"<<endl;
+    cout<<"Enter the first number :"<<endl;
+    int n1, n2, ans;
+    cin>>n1;
+    cout<<"Enter the second no "<<endl;
+    cin>>n2;
+    ans=n1+n2;
+    cout<<"Addition is "<<ans;
+    return 0;
+}
